bd86223c6a9c206ad6ae616bd50a3d2b786fa3d9
YukinaMochizuki <1p41p4jejo@gmail.com>
2021-08-21 13:06:55 +0800

    Remove catch IOException in test

--- src/test/java/tw/yukina/notion/sdk/model/block/ParagraphBlockTest.java
+++ src/test/java/tw/yukina/notion/sdk/model/block/ParagraphBlockTest.java
@@ -7,7 +7,6 @@ import tw.yukina.notion.sdk.model.common.rich.MentionText;
 import tw.yukina.notion.sdk.model.common.rich.RichText;
 import tw.yukina.notion.sdk.model.common.rich.RichTextHelper;
 import tw.yukina.notion.sdk.model.common.rich.mention.UserMention;
-import tw.yukina.notion.sdk.model.common.user.Person;
 import tw.yukina.notion.sdk.model.common.user.PersonUser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -35,48 +34,57 @@ class ParagraphBlockTest extends ModelTest {
 
     @Test
     @SuppressWarnings("SpellCheckingInspection")
-    void pageMentionTest(){
-        try {
-            Response response = getResponse( BASE_URL + "/blocks/a8eadea3f7f34a9d9425431cec8ff118");
-            Block block = readValueUseCommonObjectMapper(response, Block.class);
+    void pageMentionTest() throws IOException {
+        Response response = getResponse( BASE_URL + "/blocks/a8eadea3f7f34a9d9425431cec8ff118");
+        Block block = readValueUseCommonObjectMapper(response, Block.class);
 
-            String uuid = "8d0f791d-c0fc-4ee6-bc7f-27fe0a623cad";
-            String href = "https://www.notion.so/" + uuid.replace("-", "");
-            RichText text = RichTextHelper.createPageMention("Notion Java SDK (ReadOnly)", uuid);
-            text.setHref(new URL(href));
+        String uuid = "8d0f791d-c0fc-4ee6-bc7f-27fe0a623cad";
+        String href = "https://www.notion.so/" + uuid.replace("-", "");
+        RichText text = RichTextHelper.createPageMention("Notion Java SDK (ReadOnly)", uuid);
+        text.setHref(new URL(href));
 
-            Block pageMentionBlock = BlockHelper.createDefaultParagraph(text);
-            pageMentionBlock.setId("a8eadea3-f7f3-4a9d-9425-431cec8ff118");
+        Block pageMentionBlock = BlockHelper.createDefaultParagraph(text);
+        pageMentionBlock.setId("a8eadea3-f7f3-4a9d-9425-431cec8ff118");
 
-            assertEquals(block, pageMentionBlock);
-            response.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        assertEquals(block, pageMentionBlock);
+        response.close();
     }
 
     @Test
     @SuppressWarnings("SpellCheckingInspection")
-    void personMentionTest(){
-        try {
-            Response response = getResponse( BASE_URL + "/blocks/302cd1a8c9d3413c875a167fed0cdbb0");
-            Block block = readValueUseCommonObjectMapper(response, Block.class);
+    void personMentionTest() throws IOException {
+        Response response = getResponse( BASE_URL + "/blocks/302cd1a8c9d3413c875a167fed0cdbb0");
+        Block block = readValueUseCommonObjectMapper(response, Block.class);
 
-            String uuid = "4640eada-22ca-47be-9093-07524a2e777b";
-            String avatarUrl = "https://s3-us-west-2.amazonaws.com/public.notion-static.com/1978afb7-35bb-4111-9d2f-3c67c7d00b13/FB_IMG_1594677191662_(1).jpg";
-            MentionText mentionText = RichTextHelper.createPersonMention("知望月", uuid);
-            UserMention userMention = (UserMention) mentionText.getMention();
-            PersonUser personUser = (PersonUser) userMention.getUser();
-            personUser.setAvatar(new URL(avatarUrl));
-            personUser.getPerson().setEmail("1p41p4jejo@gmail.com");
+        String uuid = "4640eada-22ca-47be-9093-07524a2e777b";
+        String avatarUrl = "https://s3-us-west-2.amazonaws.com/public.notion-static.com/1978afb7-35bb-4111-9d2f-3c67c7d00b13/FB_IMG_1594677191662_(1).jpg";
+        MentionText mentionText = RichTextHelper.createPersonMention("知望月", uuid);
+        UserMention userMention = (UserMention) mentionText.getMention();
+        PersonUser personUser = (PersonUser) userMention.getUser();
+        personUser.setAvatar(new URL(avatarUrl));
+        personUser.getPerson().setEmail("1p41p4jejo@gmail.com");
 
-            Block userMentionBlock = BlockHelper.createDefaultParagraph(mentionText);
-            userMentionBlock.setId("302cd1a8-c9d3-413c-875a-167fed0cdbb0");
+        Block userMentionBlock = BlockHelper.createDefaultParagraph(mentionText);
+        userMentionBlock.setId("302cd1a8-c9d3-413c-875a-167fed0cdbb0");
 
-            assertEquals(block, userMentionBlock);
-            response.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        assertEquals(block, userMentionBlock);
+        response.close();
+    }
+
+    @Test
+    void databaseMentionTest() throws IOException {
+        Response response = getResponse(BASE_URL + "/blocks/59f3239743fe41d5b61e978b540721b4");
+        Block block = readValueUseCommonObjectMapper(response, Block.class);
+
+        String uuid = "8140870d-e48d-4e3d-8496-1647c7810b11";
+        String href = "https://www.notion.so/" + uuid.replace("-", "");
+        RichText text = RichTextHelper.createDatabaseMention("Test DB", uuid);
+        text.setHref(new URL(href));
+
+        Block pageMentionBlock = BlockHelper.createDefaultParagraph(text);
+        pageMentionBlock.setId("59f32397-43fe-41d5-b61e-978b540721b4");
+
+        assertEquals(block, pageMentionBlock);
+        response.close();
     }
 }
