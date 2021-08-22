4e5b0be7654040b8986512853219bf8e5e780d37
YukinaMochizuki <1p41p4jejo@gmail.com>
2021-08-23 00:44:45 +0800

    Add Color enum

--- src/main/java/tw/yukina/notion/sdk/model/Color.java
+++ src/main/java/tw/yukina/notion/sdk/model/Color.java
@@ -1,5 +1,37 @@
 package tw.yukina.notion.sdk.model;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Color {
+    DEFAULT("default"),
+    GRAY("gray"),
+    BROWN("brown"),
+    ORANGE("orange"),
+    YELLOW("yellow"),
+    GREEN("green"),
+    BLUE("blue"),
+    PURPLE("purple"),
+    PINK("pink"),
+    RED("red"),
+    GRAY_BACKGROUND("gray_background"),
+    BROWN_BACKGROUND("brown_background"),
+    ORANGE_BACKGROUND("orange_background"),
+    YELLOW_BACKGROUND("yellow_background"),
+    GREEN_BACKGROUND("green_background"),
+    BLUE_BACKGROUND("blue_background"),
+    PURPLE_BACKGROUND("purple_background"),
+    PINK_BACKGROUND("pink_background"),
+    RED_BACKGROUND("red_background");
+
+    private final String field;
+
+    Color(String field) {
+        this.field = field;
+    }
+
+    @JsonValue
+    public String getField(){
+        return field;
+    }
 
 }
