79a91eeb50d196f7ef160fef5fd569cff82e8266
YukinaMochizuki <1p41p4jejo@gmail.com>
2021-09-29 18:44:49 +0800

    Add child block template and appendBlockChild endpoint test

---
+++ src/test/java/tw/yukina/notion/sdk/model/template/BlockTemplate.java
@@ -0,0 +1,44 @@
+package tw.yukina.notion.sdk.model.template;
+
+import org.jetbrains.annotations.NotNull;
+import tw.yukina.notion.sdk.model.block.Block;
+import tw.yukina.notion.sdk.model.helper.BlockHelper;
+import tw.yukina.notion.sdk.model.block.list.ListBlockHelper;
+import tw.yukina.notion.sdk.model.common.rich.RichText;
+import tw.yukina.notion.sdk.model.helper.RichTextHelper;
+
+import java.util.*;
+
+public class BlockTemplate {
+
+    @NotNull
+    public static List<Block> getPageBlock() throws Exception {
+        List<Block> blocks = new ArrayList<>();
+
+        Block addBlock = BlockHelper.createDefaultParagraph("Text");
+        blocks.add(addBlock);
+
+        addBlock = ListBlockHelper
+                .createDefaultBulletedList(RichTextHelper.createDefaultArrayText("bulleted List 1"));
+        blocks.add(addBlock);
+
+        addBlock = ListBlockHelper
+                .createDefaultBulletedList(RichTextHelper.createDefaultArrayText("bulleted List 2"));
+        blocks.add(addBlock);
+
+        RichText text = RichTextHelper.createDateMention("2021-08-21");
+        addBlock = BlockHelper.createDefaultParagraph(text);
+        blocks.add(addBlock);
+
+        text = RichTextHelper.createDateMention("2021-08-10T10:00:00.000+08:00", "2021-08-28T14:23:00.000+08:00");
+        addBlock = BlockHelper.createDefaultParagraph(text);
+        blocks.add(addBlock);
+
+        text = RichTextHelper.createDefaultText("public static void main(String[] args)");
+        text.getAnnotations().setCode(true);
+        addBlock = BlockHelper.createDefaultParagraph(text);
+        blocks.add(addBlock);
+
+        return blocks;
+    }
+}
