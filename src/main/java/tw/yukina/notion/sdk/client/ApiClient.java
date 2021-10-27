c8c770a17dc25a05f1986471b954cfd3f0d0ca75
YukinaMochizuki <1p41p4jejo@gmail.com>
2021-10-27 16:15:49 +0800

    Add database and page endpoint to api client

--- src/main/java/tw/yukina/notion/sdk/client/ApiClient.java
+++ src/main/java/tw/yukina/notion/sdk/client/ApiClient.java
@@ -1,9 +1,17 @@
 package tw.yukina.notion.sdk.client;
 
 import org.jetbrains.annotations.NotNull;
+import tw.yukina.notion.sdk.endpoint.database.QueryDatabase;
 import tw.yukina.notion.sdk.model.block.Block;
+import tw.yukina.notion.sdk.model.database.Database;
 import tw.yukina.notion.sdk.model.endpoint.block.RequestChildrenBlockList;
 import tw.yukina.notion.sdk.model.endpoint.block.ResponseBlockList;
+import tw.yukina.notion.sdk.model.endpoint.database.RequestCreateDatabase;
+import tw.yukina.notion.sdk.model.endpoint.database.RequestUpdateDatabase;
+import tw.yukina.notion.sdk.model.endpoint.database.query.ResponsePageList;
+import tw.yukina.notion.sdk.model.endpoint.page.RequestCreatePage;
+import tw.yukina.notion.sdk.model.endpoint.page.RequestUpdatePage;
+import tw.yukina.notion.sdk.model.page.Page;
 
 public interface ApiClient {
 
@@ -19,4 +27,17 @@ public interface ApiClient {
 
     Block deleteBlock(@NotNull String uuid);
 
+    Database CreateDatabase(@NotNull RequestCreateDatabase requestCreateDatabase);
+
+    ResponsePageList QueryDatabase(@NotNull String uuid, @NotNull QueryDatabase queryDatabase);
+
+    Database RetrieveDatabase(@NotNull String uuid);
+
+    Database UpdateDatabase(@NotNull String uuid, @NotNull RequestUpdateDatabase requestUpdateDatabase);
+
+    Page CreatePage(@NotNull RequestCreatePage requestCreatePage);
+
+    Page RetrievePage(@NotNull String uuid);
+
+    Page UpdatePage(@NotNull String uuid, @NotNull RequestUpdatePage requestUpdatePage);
 }
--- src/main/java/tw/yukina/notion/sdk/client/ApiClientImpl.java
+++ src/main/java/tw/yukina/notion/sdk/client/ApiClientImpl.java
@@ -9,9 +9,23 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import org.jetbrains.annotations.NotNull;
 import tw.yukina.notion.sdk.endpoint.block.*;
+import tw.yukina.notion.sdk.endpoint.database.CreateDatabase;
+import tw.yukina.notion.sdk.endpoint.database.QueryDatabase;
+import tw.yukina.notion.sdk.endpoint.database.RetrieveDatabase;
+import tw.yukina.notion.sdk.endpoint.database.UpdateDatabase;
+import tw.yukina.notion.sdk.endpoint.page.CreatePage;
+import tw.yukina.notion.sdk.endpoint.page.RetrievePage;
+import tw.yukina.notion.sdk.endpoint.page.UpdatePage;
 import tw.yukina.notion.sdk.model.block.Block;
+import tw.yukina.notion.sdk.model.database.Database;
 import tw.yukina.notion.sdk.model.endpoint.block.RequestChildrenBlockList;
 import tw.yukina.notion.sdk.model.endpoint.block.ResponseBlockList;
+import tw.yukina.notion.sdk.model.endpoint.database.RequestCreateDatabase;
+import tw.yukina.notion.sdk.model.endpoint.database.RequestUpdateDatabase;
+import tw.yukina.notion.sdk.model.endpoint.database.query.ResponsePageList;
+import tw.yukina.notion.sdk.model.endpoint.page.RequestCreatePage;
+import tw.yukina.notion.sdk.model.endpoint.page.RequestUpdatePage;
+import tw.yukina.notion.sdk.model.page.Page;
 
 @Getter
 @Setter
@@ -23,9 +37,8 @@ public class ApiClientImpl implements ApiClient{
 
     private SimpleModule objectMapperModule;
 
-    @Getter(AccessLevel.PROTECTED)
-    @Setter(AccessLevel.PRIVATE)
-    private String authorization;
+    @Getter(AccessLevel.PRIVATE)
+    private final String authorization;
 
     private String notionVersion;
 
@@ -68,5 +81,40 @@ public class ApiClientImpl implements ApiClient{
     @Override
     public Block deleteBlock(@NotNull String uuid) {
         return DeleteBlock.callValue(uuid, getOkHttpClient(), getRequestBuilder(), getObjectMapper());
+    }
+
+    @Override
+    public Database CreateDatabase(@NotNull RequestCreateDatabase requestCreateDatabase) {
+        return CreateDatabase.callValue(requestCreateDatabase, getOkHttpClient(), getRequestBuilder(), getObjectMapper());
+    }
+
+    @Override
+    public ResponsePageList QueryDatabase(@NotNull String uuid, @NotNull QueryDatabase queryDatabase) {
+        return QueryDatabase.callValue(uuid, queryDatabase, getOkHttpClient(), getRequestBuilder(), getObjectMapper());
+    }
+
+    @Override
+    public Database RetrieveDatabase(@NotNull String uuid) {
+        return RetrieveDatabase.callValue(uuid, getOkHttpClient(), getRequestBuilder(), getObjectMapper());
+    }
+
+    @Override
+    public Database UpdateDatabase(@NotNull String uuid, @NotNull RequestUpdateDatabase requestUpdateDatabase) {
+        return UpdateDatabase.callValue(uuid, requestUpdateDatabase, getOkHttpClient(), getRequestBuilder(), getObjectMapper());
+    }
+
+    @Override
+    public Page CreatePage(@NotNull RequestCreatePage requestCreatePage) {
+        return CreatePage.callValue(requestCreatePage, getOkHttpClient(), getRequestBuilder(), getObjectMapper());
+    }
+
+    @Override
+    public Page RetrievePage(@NotNull String uuid) {
+        return RetrievePage.callValue(uuid, getOkHttpClient(), getRequestBuilder(), getObjectMapper());
+    }
+
+    @Override
+    public Page UpdatePage(@NotNull String uuid, @NotNull RequestUpdatePage requestUpdatePage) {
+        return UpdatePage.callValue(uuid, requestUpdatePage, getOkHttpClient(), getRequestBuilder(), getObjectMapper());
     }
 }
