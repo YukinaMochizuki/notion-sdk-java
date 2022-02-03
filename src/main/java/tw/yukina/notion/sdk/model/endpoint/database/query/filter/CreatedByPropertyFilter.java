10f3c39c89f68e4d435412f76764e937aadbeba0
Yukina Mochizuki <1p41p4jejo@gmail.com>
2022-02-03 23:46:28 +0800

    Add static factory to CreatedByPropertyFilter

--- src/main/java/tw/yukina/notion/sdk/model/endpoint/database/query/filter/CreatedByPropertyFilter.java
+++ src/main/java/tw/yukina/notion/sdk/model/endpoint/database/query/filter/CreatedByPropertyFilter.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.*;
+import org.jetbrains.annotations.NotNull;
 
 @Getter
 @Setter
@@ -18,4 +19,12 @@ public class CreatedByPropertyFilter extends DatabasePropertyFilter {
 
     @JsonProperty(CREATED_BY_FIELD)
     private RelationFilterObject relationFilterObject;
+
+    @NotNull
+    public static CreatedByPropertyFilter of(String property, RelationFilterObject relationFilterObject){
+        CreatedByPropertyFilter createdByPropertyFilter = new CreatedByPropertyFilter();
+        createdByPropertyFilter.setName(property);
+        createdByPropertyFilter.setRelationFilterObject(relationFilterObject);
+        return createdByPropertyFilter;
+    }
 }
